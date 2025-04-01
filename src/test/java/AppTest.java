ee3262b18a93a020c64038b6a8114e76db2900cc
hemanthv20 <144247131+hemanthv20@users.noreply.github.com>
2025-04-01 09:56:15 +0530

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,8 +2,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-  @Test
-  public void testExample() {
-    assertTrue(true); // Replace with actual tests
-  }
+    @Test  // <- This annotation is REQUIRED
+    public void sampleTest() {
+        assertTrue(true);  // Simple passing test
+    }
 }
