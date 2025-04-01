6cf8fa288a327ab9916d7c5f3fffe4c7a04514a3
hemanthv20 <144247131+hemanthv20@users.noreply.github.com>
2025-04-01 09:43:21 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI!");
+    }
+
+    // A simple method to demonstrate testing
+    public static String getGreeting() {
+        return "Hello, Jenkins CI!";
+    }
+}
